0a3b87337b4f1c80b819e3f9d70ddf9495cd0ea7
Thais-DN <142531680+Thais-DN@users.noreply.github.com>
2024-02-28 11:33:54 -0300

    responsividade e ajuste no layout

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
-
-
-const key = "4dfc67179aa0bc47ce1c1b1fbde8c354"
+const key = "4dfc67179aa0bc47ce1c1b1fbde8c354";
 
 function mostrarDados(dados) {
-    console.log(dados)
-    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
-    document.querySelector(".temperatura").innerHTML = Math.floor(dados.main.temp) + "°C"
-    document.querySelector(".txt_previsao").innerHTML = dados.weather[0].description
-    document.querySelector(".umidade").innerHTML = dados.main.humidity + "%"
-    document.querySelector(".img-previsao").src = `img/${dados.weather[0].icon}.png`
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name;
+    document.querySelector(".temperatura").innerHTML =
+        Math.floor(dados.main.temp) + "°C";
+    document.querySelector(".txt_previsao").innerHTML =
+        dados.weather[0].description;
+    document.querySelector(".umidade").innerHTML =
+        "umidade " + dados.main.humidity + "%";
 }
 
 async function buscarCidade(cidade) {
-
-    const dados = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(resposta => resposta.json())
-
-    mostrarDados(dados)
+    try {
+        const resposta = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`
+        );
+        if (!resposta.ok) {
+            throw new Error("Falha na busca da cidade");
+        }
+        const dados = await resposta.json();
+        mostrarDados(dados);
+    } catch (erro) {
+        console.error("Erro ao buscar dados da cidade:", erro);
+    }
 }
 
 function pesquisarCidade() {
-    const cidade = document.querySelector(".input_cidade").value
+    const cidade = document.querySelector(".input_cidade").value;
 
-    buscarCidade(cidade)
+    buscarCidade(cidade);
 }
